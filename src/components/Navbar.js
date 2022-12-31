f76b371e84bf36859ea8681292958c8da124a296
SifatNiloy <sifatniloy18@gmail.com>
2022-12-31 02:09:52 -0800

    navbar task doing

---
+++ src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Nav, NavDropdown } from 'react-bootstrap';
+
+const Navbar = () => {
+    const [activeLink, setActiveLink]= useState('home')
+    const [scrolled, setScrolled]= useState(false)
+    useEffect(()=>{
+        const onScroll=()=>{
+            if(window.scrollY){
+                setScrolled(true)
+            }
+            else{
+                setScrolled(false)
+            }
+        }
+        window.addEventListener('scroll', onScroll);
+        return()=> window.removeEventListener("scroll", onScroll)
+    },[])
+    return (
+        <Navbar expand="lg" className={scrolled? "scrolled":""}>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img src='' alt='logo'></img>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav">
+                    <span className='navbar-toogler-icon'> </span>
+                </Navbar.Toggle>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>                        
+                    </Nav>
+                    <span className='navbar-text'>
+                        <div className='social-icon'>
+                            <a href='#'> <img src='' alt=''></img> </a>
+                            <a href='#'> <img src='' alt=''></img> </a>
+                            <a href='#'> <img src='' alt=''></img> </a>
+                        </div>
+                        <button className='vvd ' onClick={()=>console.log('connect')}> <span>Let's connect</span> </button> 
+                    </span>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Navbar;
