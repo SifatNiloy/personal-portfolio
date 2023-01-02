4b6fdfeaf8f33fff1f58de6e976c57634211f00c
SifatNiloy <sifatniloy18@gmail.com>
2023-01-02 00:02:07 -0800

    half done

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,58 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import headerImg from '../assets/img/header-img.svg'
-const Banner = () => {
-    const [loopNum , setloopNum]= useState(0);
-    const [isDeleting, setIsDeleting]= useState(false);
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
-    const toRotate=["Web developer", "Web designer", "UI/UX designer"]
-    const [text, setText]= useState('');
-    const [delta, setDelta]= useState(3000- Math.random()*100)
-    const period= 2000;
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Fullstack Developer", "Frontend Developer", "Frontend Developer" ];
+  const period = 100;
 
-    useEffect(()=>{
-        let ticker= setInterval(()=>{
-            tick();
-        },delta)
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
 
-        return ()=>{ clearInterval (ticker)}
-    }, [text ])
+    return () => { clearInterval(ticker) };
+  }, [text])
 
-    const tick= ()=>{
-        let i= loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting? fullText.substring(0, text.length-1): fullText.substring(0, text.length+1)
-        setText(updatedText);
-        if(isDeleting){
-            setDelta(prevDelta=> prevDelta/2)
-        }
-        if(!isDeleting && updatedText=== fullText){
-            setIsDeleting(true)
-            setDelta(period)
-        }
-        else if(isDeleting && updatedText===''){
-            setIsDeleting(false);
-            setloopNum(loopNum+1);
-            setDelta(500)
-        }
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
     }
-    return (
-        <section className='banner' id='home'>
-            <Container>
-                <Row className='align-items-center'>
-                    <Col xs={12} md={6} xl={7}>
-                        <span className='tagline'>Welcome to my Portfolio</span>
-                        <h1>{`Hi I'm web decoded`} <span className='wrap'>{text}</span> </h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, nisi. Reprehenderit iusto, sunt unde magni officiis illum sit deleniti nihil quibusdam laborum, veniam, cum vel veritatis labore esse asperiores nesciunt?</p>
-                        <button onClick={() => console.log('connect')}>Let's Connect <i class="bi bi-arrow-right-circle"></i></button>
-                    </Col>
-                    <Col xs={12} md={6} xl={5}>
-                        <img src={headerImg} alt="header img" />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    );
-};
 
-export default Banner;
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm sifat`} <br /> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Hello! I am Sifat, a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
