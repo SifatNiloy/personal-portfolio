4acfce65a10ba996d9616dcfa08bf8781c0c940a
SifatNiloy <sifatniloy18@gmail.com>
2023-01-03 15:44:11 -0800

    icon and link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,8 @@ export const Footer = () => {
             <img src={myLogo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
-            <div className="social-icon">
+            <div className="social-icon pt-5">
+              
               <a href="#"><img src={navIcon1} alt="Icon" /></a>
               <a href="#"><img src={navIcon2} alt="Icon" /></a>
               <a href="#"><img src={navIcon3} alt="Icon" /></a>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,11 +51,12 @@ export const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/sifat-niloy/"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://github.com/SifatNiloy"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.behance.net/sifat_niloy"><img src={navIcon3} alt="" /></a>
+                
               </div>
               <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span>Let's Connect</span></button>
               </HashLink>
             </span>
           </Navbar.Collapse>
